132e6ed698d3894a3b2591e71b844f0c5791acfe
asifgenius <asif15-11288@diu.edu.bd>
2024-08-23 21:56:25 +0600

    Add increase database time out

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 if (process.env.NODE_ENV === 'development') {
-  mongoose.connect(process.env.DATABASE_URL)
+  mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000,
+    socketTimeoutMS: 45000, 
+    bufferCommands: false,
+  })
     .then(() => {
       console.log('Connected to database');
     })
